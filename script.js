a58182106c5a56c1854c1fb1e5ddc615f6040f92
AndreCostaAIB <36609840+AndreCostaAIB@users.noreply.github.com>
2018-10-02 22:25:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+alert("Este é o meu pedido de Amadrinhamento à Doutoura.");
+alert("De: Besta Bestial.");
+var confirmar = confirm("Esta página vai ser redirecionada para o YouTube.");
+if (confirmar = true){window.location.replace("https://www.youtube.com/watch?time_continue=13&v=gt0cmgkzzlA")};
